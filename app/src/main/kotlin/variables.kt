53f4905ee851f2a56fed49c1e38bfcdc3c679c6f
Abusiq <dameyler@gmail.com>
2025-08-20 12:32:20 +0300

    variables

--- app/src/main/kotlin/variables.kt
+++ app/src/main/kotlin/variables.kt
@@ -2,7 +2,7 @@ package org.example.app
 
 fun main()  {
     val numberOfOrders = 75
-    val message = "Спасибо за покупку!"
+    val message = "Спасибо за покупку"
 
     println(numberOfOrders)
     println(message)
