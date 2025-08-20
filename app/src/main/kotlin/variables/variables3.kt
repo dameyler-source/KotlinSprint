10210aaabf0536532655f23ff005b857d88c8371
Abusiq <dameyler@gmail.com>
2025-08-20 13:47:51 +0300

    KS-1-3-variables

---
+++ app/src/main/kotlin/variables/variables3.kt
@@ -0,0 +1,17 @@
+package org.example.app.variables
+
+fun main() {
+    val year = 1961
+    var hour: Int = 9
+    var minute: Int = 7
+
+    println(year)
+//    println(hour)
+//    println(minute)
+
+    hour = 10
+    minute = 55
+
+    println("$hour:$minute")
+
+}
