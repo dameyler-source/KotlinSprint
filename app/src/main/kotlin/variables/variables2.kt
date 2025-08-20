420f6453616eaa0c1134b8faaca707efe7772466
Abusiq <dameyler@gmail.com>
2025-08-20 13:02:52 +0300

    KS-1-2-variables

---
+++ app/src/main/kotlin/variables/variables2.kt
@@ -0,0 +1,15 @@
+package org.example.app.variables
+
+fun main()  {
+    val numberOfOrders = 75
+    val message = "Спасибо за покупку!"
+
+    println(numberOfOrders)
+    println(message)
+
+//    val numberOfWorkers = 2000
+//    println(numberOfWorkers)
+
+    val numberOfWorkers = 1999
+    println(numberOfWorkers)
+}
