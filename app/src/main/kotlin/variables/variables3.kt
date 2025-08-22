e67ed1305537a4ff4decb8da12ee86bd34218fa3
Abusiq <dameyler@gmail.com>
2025-08-22 10:00:27 +0300

    KS-1-3-variables

--- app/src/main/kotlin/variables/variables3.kt
+++ app/src/main/kotlin/variables/variables3.kt
@@ -2,15 +2,14 @@ package org.example.app.variables
 
 fun main() {
     val year = 1961
-    var hour: Int = 9
-    var minute: Int = 7
+    var hour: String = "09"
+    var minute: String = "07"
 
     println(year)
-//    println(hour)
-//    println(minute)
+    println("$hour:$minute")
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
 
     println("$hour:$minute")
 }
