e6d82d0f58aa53afa52dfc53db28daa7c3c8cd89
Abusiq <dameyler@gmail.com>
2025-08-22 15:09:13 +0300

    KS-1-4-variables

---
+++ app/src/main/kotlin/variables/variables4.kt
@@ -0,0 +1,19 @@
+package org.example.app.variables
+
+fun main() {
+
+    val lenght: Long = 40_868_600_000
+    val age: Short = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println(lenght)
+    println(age)
+    println(partOfDay)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+
+}
