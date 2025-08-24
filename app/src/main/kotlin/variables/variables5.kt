4279db3e2f5c5f032b315711c2fae9b376f81561
Abusiq <dameyler@gmail.com>
2025-08-24 12:16:57 +0300

    KS-1-5-variables

--- app/src/main/kotlin/variables/variables5.kt
+++ app/src/main/kotlin/variables/variables5.kt
@@ -2,10 +2,12 @@ package org.example.app.variables
 
 fun main() {
 
-    val seconds = "00"
-    val minutes = 48
-    val hour = "01"
+    val totalSeconds = 6480
 
-    println("$hour:$minutes:$seconds")
+    val hours = totalSeconds / 3600
+    val minutes = (totalSeconds % 3600) / 60
+    val seconds = totalSeconds % 60
+
+    println("$hours:$minutes:$seconds")
 
 }
