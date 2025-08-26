576880183c374cd77286c9a6c82c017f67669c95
Abusiq <dameyler@gmail.com>
2025-08-26 16:08:26 +0300

    KS-2-1 do average grades

--- app/src/main/kotlin/lesson_2/KS-2-1.kt
+++ app/src/main/kotlin/lesson_2/KS-2-1.kt
@@ -3,8 +3,12 @@ package lesson_2
 fun main() {
 
     val numberOfStudents = 4
-    val grades = 3 + 4 + 3 + 5
-    val average: Float = (grades / numberOfStudents).toFloat()
+    val grade1 = 3
+    val grade2 = 4
+    val grade3 = 3
+    val grade4 = 5
+    val grades: Float = (grade1 + grade2 + grade3 + grade4).toFloat()
+    val average = ((grades) / numberOfStudents)
 
     println(average)
 
