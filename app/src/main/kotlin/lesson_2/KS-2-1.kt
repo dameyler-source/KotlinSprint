53941bb28c0544f98459677a8b5ea2a8e018c934
Abusiq <dameyler@gmail.com>
2025-08-26 14:05:40 +0300

    KS-2-1-AO

--- app/src/main/kotlin/lesson_2/KS-2-1.kt
+++ app/src/main/kotlin/lesson_2/KS-2-1.kt
@@ -3,7 +3,8 @@ package lesson_2
 fun main() {
 
     val numberOfStudents = 4
-    val studentsGrades: Float = ((3 + 4 + 3 + 5) / 4f)
+    val studentsGrades: Float = (3 + 4 + 3 + 5) / 4f
 
     println(studentsGrades)
+
 }
